617db11af95511fc50de9fd109e08c5ce46a60ae
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-04-17 11:15:55 +0200

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -17,6 +17,13 @@ def log():
         raise(e)
     return(jsonify({'result':'ok'}))
 
+#added for display
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def honey(path):
+    log_request(request)
+    return jsonify({'result': 'Receiver ok'})
+#added for display end
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080, debug=True)
